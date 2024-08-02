1465177bb9b320d7a01f37053c7e12819cde19ab
Bruno Vieira Nobre <104945877+BrunoV7@users.noreply.github.com>
2024-08-02 17:47:04 -0300

    Correção da area de login, Correção da area de perfil

--- src/app/components/layout/sidebar/sidebar.component.ts
+++ src/app/components/layout/sidebar/sidebar.component.ts
@@ -1,62 +1,61 @@
-import {Component, ElementRef, inject, ViewChild} from '@angular/core';
-import {Router} from "@angular/router";
+import { Component, ElementRef, inject, ViewChild } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-sidebar',
+  selector: "app-sidebar",
   standalone: true,
   imports: [],
-  templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.scss'
+  templateUrl: "./sidebar.component.html",
+  styleUrl: "./sidebar.component.scss",
 })
 export class SidebarComponent {
-  @ViewChild('sidebar') sidebar!: ElementRef;
+  @ViewChild("sidebar") sidebar!: ElementRef;
 
   router = inject(Router);
 
   construtor() {}
 
-  toggle(){
-    this.sidebar.nativeElement.classList.toggle('open-sidebar');
+  toggle() {
+    this.sidebar.nativeElement.classList.toggle("open-sidebar");
   }
 
-  nav(option: number){
-    switch(option){
+  nav(option: number) {
+    switch (option) {
       case 0:
-        this.router.navigate(['/user/dashboard']);
+        this.router.navigate(["/user/dashboard"]);
         break;
 
       case 1:
-        this.router.navigate(['/user/cards']);
+        this.router.navigate(["/user/cards"]);
         break;
 
       case 2:
-        this.router.navigate(['/user/income']);
+        this.router.navigate(["/user/income"]);
         break;
 
       case 3:
-        this.router.navigate(['/user/expanses']);
+        this.router.navigate(["/user/expanses"]);
         break;
 
       case 4:
-        this.router.navigate(['/user/banks']);
+        this.router.navigate(["/user/banks"]);
         break;
 
       case 5:
-        this.router.navigate(['/user/goals']);
+        this.router.navigate(["/user/goals"]);
         break;
 
       case 6:
-        this.router.navigate(['/user/dashboard']);
+        this.router.navigate(["/user/dashboard"]);
         break;
 
       case 7:
-        this.router.navigate(['/user/dashboard']);
+        this.router.navigate(["/user/dashboard"]);
         break;
 
       case 8:
-        this.router.navigate(['/user/profile']);
+        this.router.navigate(["/user/profile"]);
         break;
     }
   }
-
 }
